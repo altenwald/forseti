ca7cbe90145bcd1fd1e6fb3d91bbd18b5d5eb069
Manuel Rubio <manuel.rubio@veon.com>
2017-12-08 10:12:35 +0100

    improving compatibility with OTP 17+ (about dict() types)

--- include/forseti.hrl
+++ include/forseti.hrl
@@ -1,10 +1,10 @@
 -define(MAX_RETRIES, 20).
 -define(MAX_TIME, 10).
 
--ifdef(NEW_DICT_TYPE).
--define(DICT_TYPE, (dict:dict())).
--else.
+-ifdef(OLD_DICT_TYPE).
 -define(DICT_TYPE, (dict())).
+-else.
+-define(DICT_TYPE, (dict:dict())).
 -endif.
 
 -type call() :: {module(), atom(), [term()]}.
